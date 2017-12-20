34809b1cc9656801f9141687fe75a82e9c7953b8
Alec Tossani <alecatjackrabbit@MacBook-Pro.local>
2017-12-19 22:29:35 -0700

    Project Euler problem 25 solution.

--- ProjectEuler.hs
+++ ProjectEuler.hs
@@ -284,4 +284,9 @@ The 12th term, F12, is the first term to contain three digits.
 
 What is the index of the first term in the Fibonacci sequence to contain 1000 digits?--}
 
--- euler25
+euler25 :: Integer -> Integer
+euler25 n = ff n fibs 2
+  where
+    ff n (f:fs) accum | len2 f == n = accum
+                      | otherwise = ff n fs (accum + 1)
+
